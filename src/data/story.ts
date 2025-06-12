993cd5d791f2bebbf7c8be347cdab43f47ee5e48
hiiragi17x <hiiragi.en22@gmail.com>
2025-06-12 23:02:17 +0900

    fix:物語修正

--- src/data/story.ts
+++ src/data/story.ts
@@ -1,5 +1,14 @@
 export const storyData = {
   "start": {
+    "text": "ここは片田舎のとある会社。どうしようもない上司と訳のわからない人しかいない、変人の巣窟。給料は安く、残業時間は多い。こんな会社辞めてやりたい。部長が声をかけてきた。よし殴ろう。",
+    "background": "/images/bg/office.jpg",
+    "bgm": "/bgm/n43.mp3",
+    "choices": [
+      { "text": "出社する", "next": "overwork-bad" },
+      { "text": "逃げる", "next": "freedom-good" }
+    ]
+  },
+  "unobtrusive": {
     "text": "ここは片田舎のとある会社。どうしようもない上司と訳のわからない人しかいない、変人の巣窟。給料は安く、残業時間は多い。こんな会社辞めてやりたい。どうしますか？",
     "background": "/images/bg/office.jpg",
     "bgm": "/bgm/n43.mp3",
